77cc4577f8810a12cc31b7d65232aa4483d4e33a
Matt Peters <matthew.george.peters@gmail.com>
2020-09-30 15:54:05 -0400

    wrapped App in Provider HOC

--- src/index.js
+++ src/index.js
@@ -12,9 +12,16 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './components/App.jsx';
+import store from './state/store';
 
 import styles from './styles/styles.scss';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
