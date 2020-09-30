d87bd49a13cb1dd2f8f8c6312d63e8e30c1402ec
Matt Peters <matthew.george.peters@gmail.com>
2020-09-30 16:48:17 -0400

    added saga boiler and composed saga middleware with composeDevTools

---
+++ src/state/sagas/saga.js
@@ -0,0 +1,10 @@
+/**
+ * ************************************
+ *
+ * @module  saga.js
+ * @author  mpeters
+ * @date    09/30/2020
+ * @description Redux-saga middleware
+ *
+ * ************************************
+ */
--- src/state/store.js
+++ src/state/store.js
@@ -5,14 +5,28 @@
  * @author  mpeters
  * @date    09/30/2020
  * @description Redux 'single source of truth'
+ * additions include: redux-saga and enabling use
+ * of redux dev-tools
  *
  * ************************************
  */
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import reducer from './reducers/weatherDataReducer';
 
-const store = createStore(reducer, composeWithDevTools());
+import reducer from './reducers/weatherDataReducer';
+import saga from './sagas/saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+// createStore only takes one 'enhancer function', combined applyMiddleware() and
+// composeWithDevTools() with 'compose'.
+const store = createStore(
+  reducer,
+  compose(applyMiddleware(sagaMiddleware), composeWithDevTools())
+);
+
+sagaMiddleware.run(saga);
 
 export default store;
