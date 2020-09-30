8b01223511a424166a6e84b527a53600243efe6f
Matt Peters <matthew.george.peters@gmail.com>
2020-09-29 20:44:43 -0400

    testing weather api, created locations file for obj export

---
+++ src/util/locations.js
@@ -0,0 +1,32 @@
+export default {
+  newyork: {
+    id: 5128581,
+    name: 'New York City',
+    state: 'NY',
+    country: 'US',
+    coord: {
+      lon: -74.005966,
+      lat: 40.714272,
+    },
+  },
+  losangeles: {
+    id: 5368361,
+    name: 'Los Angeles',
+    state: 'CA',
+    country: 'US',
+    coord: {
+      lon: -118.243683,
+      lat: 34.052231,
+    },
+  },
+  miami: {
+    id: 4164138,
+    name: 'Miami',
+    state: 'FL',
+    country: 'US',
+    coord: {
+      lon: -80.193657,
+      lat: 25.774269,
+    },
+  },
+};
