62dcbd9b7d3f0a38a1de272e2a1d9dda87550150
Matt Peters <matthew.george.peters@gmail.com>
2020-10-05 19:08:46 -0400

    Nav tests completed

--- __tests__/src/ui/components/containers/App.test.jsx
+++ __tests__/src/ui/components/containers/App.test.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// import configureStore from 'redux-mock-store';
+
 import App from '../../../../../src/ui/containers/App.jsx';
-// import Adapter from 'enzyme-adapter-react-16';
-// const mockStore = configureStore([]);
 
 describe('App container', () => {
-  // const store = mockStore({
-  //   yaml: {
-  //     fetchedContent: true,
-  //     sections: { byId: []},
-  //   }
-  // });
-
-  const wrapper = shallow(<App />).find('App');
+  const wrapper = shallow(<App />);
 
   it('matches previous snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('renders a Nav component', () => {
-    const hero = wrapper.find('Nav');
-    expect(hero).toHaveLength(1);
+  it('should have a 1 section', () => {
+    const section = wrapper.find('section');
+    expect(section).toHaveLength(1);
   });
 });
