c4f60d5d3ff64e837f59cd063310a03a84f562df
Matt Peters <matthew.george.peters@gmail.com>
2020-10-05 17:51:39 -0400

    cleaned up index and renamed index scss

--- src/index.js
+++ src/index.js
@@ -15,12 +15,11 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import ProviderWrapper from './ui/wrappers/ProviderWrapper.jsx';
-// import RouterWrapper from './ui/wrappers/RouterWrapper.jsx';
 
 import App from './ui/containers/App.jsx';
 import store from './state/store';
 
-import styles from './styles/styles.scss';
+import './styles/index.scss';
 
 render(
   <ProviderWrapper store={store}>
