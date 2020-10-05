4ac53f66ad6ae5e48bead3210ffdb1b661f16e32
Matt Peters <matthew.george.peters@gmail.com>
2020-10-05 11:10:10 -0400

    working stories

--- src/stories/CurrentWeather.stories.js
+++ src/stories/CurrentWeather.stories.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import CurrentWeather from '../ui/components/CurrentWeather.jsx';
 
-import styles from '../styles/components/CurrentWeather.scss';
+import { weatherIcon, formatHours, getDate } from './dummy/functions';
+import dummyData from './dummy/successfulFetch';
 
 export default {
   title: 'Components/CurrentWeather',
@@ -12,12 +12,10 @@ export default {
 
 const Template = (args) => <CurrentWeather {...args} />;
 
-export const Populated = Template.bind({});
-Populated.args = {};
-
-CurrentWeather.propTypes = {
-  currentWeather: PropTypes.object,
-  currentDate: PropTypes.func,
-  formatTime: PropTypes.func,
-  weatherIcon: PropTypes.func,
+export const SuccessfulFetch = Template.bind({});
+SuccessfulFetch.args = {
+  weatherIcon,
+  formatTime: formatHours,
+  currentDate: getDate,
+  currentWeather: dummyData.weatherData.current,
 };
